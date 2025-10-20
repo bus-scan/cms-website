f28d0a9961aa55f3186b2ec7086d7868e6012a76
Apisit Sianglert <john.apisits@gmail.com>
2025-10-21 06:36:30 +0700

    Fix lint

--- components/common/modal/index.ts
+++ components/common/modal/index.ts
@@ -1,4 +1,3 @@
 export { default as Modal } from './Modal';
 export { default as SuccessModal } from './SuccessModal';
 export { default as ConfirmModal } from './ConfirmModal';
-export type { ModalButton } from './Modal';
